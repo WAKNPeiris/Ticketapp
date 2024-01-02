91c423024da10c6021f453962556cf5fefdda320
WAKNPeiris <kasuninayanika@gmail.com>
2024-01-02 18:06:54 +0530

    create payment page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'PaymentPage.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -32,6 +33,13 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  void navigateToPaymentPage(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => PaymentPage()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,22 +64,57 @@ class HomePage extends StatelessWidget {
 
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            decoration: const InputDecoration(
-              labelText: 'Enter Price',
-              labelStyle: TextStyle(color: Colors.black), // Label color
-              border: OutlineInputBorder(
-                  // borderSide: BorderSide(color: Colors.black),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 30),
+                const Center(
+                  child: Text(
+                    'JustGo - Ticketing Amount',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black),
-              ),
-            ),
-            keyboardType: TextInputType.number,
-            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-          ),
-        ),
+                ),
+                const SizedBox(height: 30),
+                TextField(
+                  decoration: const InputDecoration(
+                    labelText: 'Enter Price',
+                    labelStyle: TextStyle(color: Colors.black), // Label color
+                    border: OutlineInputBorder(
+                        // borderSide: BorderSide(color: Colors.black),
+                        ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(
+                        RegExp(r'^\d+\.?\d{0,2}$')),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add your confirm button logic here
+                      navigateToPaymentPage(context);
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.black),
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.white),
+                    ),
+                    child: Text('Confirm'),
+                  ),
+                ),
+              ],
+            )),
       ),
     );
   }
